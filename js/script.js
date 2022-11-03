25c74648a4ebef1751d8c4b3885be7a9b7d2bb13
Aaron Abramson <aaron.hdcomps@gmail.com>
2022-11-02 19:29:31 -0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let myGreeting = "Hello Nurd!";
+console.log(myGreeting);
+const pi = 3.14;
+console.log(pi);
+//pi = 4;
+//console.log(pi);
+let myNumber = 5;
+console.log("Your number is " + myNumber);
+console.log("The min number is " + Math.min(myNumber, pi));
